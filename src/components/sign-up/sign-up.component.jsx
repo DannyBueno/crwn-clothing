4fdb99235feb152561761a76802f9066b1917a1a
DannyBueno <Buenodanny94@gmail.com>
2020-05-01 17:21:51 -0700

    implemented firebase utils, included ability to store authenticated users into firebase database

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss'; 
+
+//state class to store user input
+class SignUp extends React.Component {
+    constructor() {
+        super();
+
+        //what the user types into the fields
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+    //async function that gets event and prevents the default submitting
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        const {displayName, email, password, confirmPassword } = this.state;
+
+        //check if pasword and confirmPassword dont match 
+        if(password !== confirmPassword) {
+            alert("passwords dont match");
+            return; 
+        }
+
+        try {
+            //return user of the await
+            //destructure user
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+           //resetting the state  
+           await createUserProfileDocument(user, { displayName });
+
+           this.setState({
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+           });
+
+        } catch (error) {
+
+            console.error(error);
+        }
+    };
+
+    handleChange = event => {
+        //destructure name & value off the target
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    };
+
+    render() {
+        //destructuring values
+        const {displayName, email, password, confirmPassword } = this.state;
+        return(
+         <div className='sign-up'>  
+             <h2 className='title'> I do not have an acoount </h2>
+               <span> Sign up with email and password </span>
+               <form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput
+                    type='text'
+                    name='displayName'
+                    value={displayName}
+                    onChange={this.handleChange}
+                    label='Display Name'
+                    required
+                    />    
+                   
+                    <FormInput
+                    type='email'
+                    name='email'
+                    value={email}
+                    onChange={this.handleChange}
+                    label='Email'
+                    required
+                    />    
+                    
+                    <FormInput
+                    type='password'
+                    name='password'
+                    value={password}
+                    onChange={this.handleChange}
+                    label='Password'
+                    required
+                    />    
+                    
+                    <FormInput
+                    type='password'
+                    name='confirmPassword'
+                    value={confirmPassword}
+                    onChange={this.handleChange}
+                    label='Confirm Password'
+                    required
+                    />    
+                    <CustomButton type='submit'> SIGN UP </CustomButton>
+               </form>
+         </div>
+       )
+    }
+}
+
+export default SignUp;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,15 +4,48 @@ import 'firebase/auth';
 
 // crwn clothing Firebase configuration 
 const config = {
-    apiKey: "AIzaSyDCgzvF3s3IiXLhUo7FGeed8ye8uBjwurk",
-    authDomain: "crwn-db-63f89.firebaseapp.com",
-    databaseURL: "https://crwn-db-63f89.firebaseio.com",
-    projectId: "crwn-db-63f89",
-    storageBucket: "crwn-db-63f89.appspot.com",
-    messagingSenderId: "2787963866",
-    appId: "1:2787963866:web:f8f77215fa824a43cc46e3",
-    measurementId: "G-L80J17LYLD"
+    apiKey: 'AIzaSyDCgzvF3s3IiXLhUo7FGeed8ye8uBjwurk',
+    authDomain: 'crwn-db-63f89.firebaseapp.com',
+    databaseURL: 'https://crwn-db-63f89.firebaseio.com',
+    projectId: 'crwn-db-63f89',
+    storageBucket: 'crwn-db-63f89.appspot.com',
+    messagingSenderId: '2787963866',
+    appId: '1:2787963866:web:f8f77215fa824a43cc46e3'
+    
   };
+
+// function to take in user auth and store into DB
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  //if userAuth does not exist return
+  if(!userAuth) return;
+
+ //get back the userRef and get a snapShot
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+ //if snapSHot does not exists, create the data
+  if (!snapShot.exists) {
+    //need displayName & email from userAuth
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    
+    // creating a new user using userAuth data
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+
+    }
+  }
+  return userRef;
+};
 
 // initialize Firebase
 firebase.initializeApp(config);
